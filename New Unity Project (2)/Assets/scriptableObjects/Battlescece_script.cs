8b937d20a67ceafb10a71036ea5b952b8cc192b4
SunnyMoonDestroyerOfUranus <boris-barczyk@hotmail.com>
2020-02-06 21:38:00 +0100

    script gemaakt
    script met publics gemaakt, joa helpt later met de goede codering en aanvragen.

---
+++ New Unity Project (2)/Assets/scriptableObjects/Battlescece_script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    // using Unity.engine.list; ofzoiets
+
+    // toevoegen van alle visual components
+
+public class Battlescece_script : MonoBehaviour
+public List pokemonlijst = new list<Pokemon>;
+public Image Enemypok;
+public Image Ourpok;
+    // public display hp ofzo
+public Button;
+    // public Eventscene;
+
+
+
+
+
+    // aanvragen van de gebeurtenissen.
+
+{   void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
