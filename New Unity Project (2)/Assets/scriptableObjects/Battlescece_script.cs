15a87ff7a0e93097deb7b22b59e0afc389f4d026
joachim69 <60317486+Joachim69@users.noreply.github.com>
2020-02-12 13:00:13 +0100

    Lijst poging 2

--- New Unity Project (2)/Assets/scriptableObjects/Battlescece_script.cs
+++ New Unity Project (2)/Assets/scriptableObjects/Battlescece_script.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
     // using Unity.engine.list; ofzoiets
 
     // toevoegen van alle visual components
 
-public class Battlescece_script : MonoBehaviour
-public List pokemonlijst = new list<Pokemon>;
-public Image Enemypok;
+public class Battlescece_script : MonoBehaviour { 
+public List<Pokemon> pokemonlijst = new List<Pokemon> { };
+[SerializeField]
+private Image Enemypok;
 public Image Ourpok;
     // public display hp ofzo
-public Button;
+public Button btn;
     // public Eventscene;
 
 
@@ -19,7 +21,7 @@ public Button;
 
     // aanvragen van de gebeurtenissen.
 
-{   void Start()
+   void Start()
     {
         
     }
