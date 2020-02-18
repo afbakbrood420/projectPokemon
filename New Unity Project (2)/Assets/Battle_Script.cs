a36f2514c9fa5ee17406cf1910966c4bbdad2bce
SunnyMoonDestroyerOfUranus <boris-barczyk@hotmail.com>
2020-02-18 13:16:08 +0100

    script + sprite change
    S

---
+++ New Unity Project (2)/Assets/Battle_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+
+public class Battle_Script : MonoBehaviour
+{
+    public Image OurpokUI;
+    public Image EnemypokUI;
+    public Button button;
+    public displayBar dbOurpok;
+    public displayBar dbEnemypok;
+    public Text Eventtekst;
+    public Image Ourpok;
+    public Image Enemypok;
+
+    // public rekemachine;
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
