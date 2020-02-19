af157ff35063090d54cecb9030afb7afd0df1e64
SunnyMoonDestroyerOfUranus <boris-barczyk@hotmail.com>
2020-02-19 12:23:34 +0100

    script
    publics

--- New Unity Project (2)/Assets/Battle_Script.cs
+++ New Unity Project (2)/Assets/Battle_Script.cs
@@ -11,14 +11,28 @@ public class Battle_Script : MonoBehaviour
 {
     public Image OurpokUI;
     public Image EnemypokUI;
-    public Button button;
+    public Image Ourpoksprite;
+    public Image Enemypoksprite;
+
     public displayBar dbOurpok;
     public displayBar dbEnemypok;
     public Text Eventtekst;
-    public Image Ourpok;
-    public Image Enemypok;
+    public Button button;
+
+    //pokemon level
+    public Text LVourpok;
+    public Text LVEnemypok;
+    public Text Ourpokname;
+    public Text Enemypokname;
+
+    //public Button Moves;//
+
 
     // public rekemachine;
+    //stats
+    //types
+    // speed ourpok == speed enemypok; then random int
+
 
     void Start()
     {
