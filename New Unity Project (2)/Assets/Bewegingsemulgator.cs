7b66792813ebbe87741cdd473b2e3604be7e3e77
TiptopSamoth <59071873+TiptopSamoth@users.noreply.github.com>
2020-02-21 17:47:21 +0100

    Emulgator voor beweging
    Ik heb een emulgator aangemaakt die zorgt voor beweging, alles van mij is verwijderd dus is nog een beetje mediocre. Fix jullie bugs ik kan niks testen. Jullie zijn echt noobs je zou uberhaupt maar bugs hebben. Nee maar ff serieus fix die dingen ik kan niks

---
+++ New Unity Project (2)/Assets/Bewegingsemulgator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bewegingsemulgator : MonoBehaviour
+    
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = gameObject.GetComponent<Rigidbody2D>();
+       transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("w")) 
+        {
+            rb.AddForce(transform.up * 3);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(transform.right * 3);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(transform.up * -3);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(transform.right * -3);
+        }
+    }
+}
---
+++ New Unity Project (2)/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public 
+Rigidbody2D rb;
+
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        GameObject.Rigidbody2d.addForce(0, 10, 0);
+    }
+}
