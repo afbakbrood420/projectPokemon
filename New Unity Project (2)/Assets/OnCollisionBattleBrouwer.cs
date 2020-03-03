061eeb3887e46baf7b90af267556c6e1ee33a59b
TiptopSamoth <59071873+TiptopSamoth@users.noreply.github.com>
2020-03-03 22:01:45 +0100

    Jaja
    Ja joh

---
+++ New Unity Project (2)/Assets/OnCollisionBattleBrouwer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+private party Party;
+
+public class OnCollisionBattleBrouwer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D bc;
+        bc = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void onTriggerEnter2D(string Collider2D)
+    {
+        Party = GameObject.FindWithTag("party");
+        Party.startFight<Brouwer>;
+    }
+}
