9090c33fc3d959ee796534e2159647d7ad702622
TiptopSamoth <59071873+TiptopSamoth@users.noreply.github.com>
2020-03-05 01:36:37 +0100

    Map geoptimaliseert
    Map geoptimaliseert en Brouwer epischer maken

--- New Unity Project (2)/Assets/Bewegingsemulgator.cs
+++ New Unity Project (2)/Assets/Bewegingsemulgator.cs
@@ -11,7 +11,7 @@ public class Bewegingsemulgator : MonoBehaviour
 
     public Transform walkDirection;
     //Geeft het punt aan waar de player naar gaat lopen.
-
+    public bool freeze = false;
     public LayerMask blockDetector;
     //Eigenschap van walkDirection, checkt of er iets staat waar hij niet mag komen, bijvoorbeeld muren of voorwerpen.
 
@@ -29,90 +29,95 @@ public class Bewegingsemulgator : MonoBehaviour
 
     private void FixedUpdate()
     // Update het programma een aantal keer per seconde
-    {
-        transform.position = Vector3.MoveTowards(transform.position, walkDirection.position, speed * Time.deltaTime);
-        //Geeft directie en snelheid aan waarmee hij naar het punt gaat waar hij toe gaat lopen, met de snelheid. Transform.position is de positie van de speler, walkDirection.position is het punt waar hij naartoe gaat, en speed * Time.deltaTime zorgt voor de snelheid.
 
-        if (Vector3.Distance(transform.position, walkDirection.position) <= 0.1f)
+    {
+        if (freeze != true)
         {
 
-            if ((Input.GetAxisRaw("Horizontal")) == 1f || (Input.GetAxisRaw("Horizontal") == -1f))
-            {
-                if (!Physics2D.OverlapCircle(walkDirection.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.05f, blockDetector))
-                {
-                    walkDirection.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
+            transform.position = Vector3.MoveTowards(transform.position, walkDirection.position, speed * Time.deltaTime);
+            //Geeft directie en snelheid aan waarmee hij naar het punt gaat waar hij toe gaat lopen, met de snelheid. Transform.position is de positie van de speler, walkDirection.position is het punt waar hij naartoe gaat, en speed * Time.deltaTime zorgt voor de snelheid.
 
-            }
-
-            else if ((Input.GetAxisRaw("Vertical")) == 1f || (Input.GetAxisRaw("Vertical") == -1f))
+            if (Vector3.Distance(transform.position, walkDirection.position) <= 0.1f)
             {
-                if (!Physics2D.OverlapCircle(walkDirection.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.05f, blockDetector))
-                {
-                    walkDirection.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                }
-            }
 
-            //check For Doors
-            if (Physics2D.OverlapCircle(transform.position, 0.05f, SceneShifter))
-            {
-                //omdat wij geen gameobject van de deur kunnen krijgen uit deze if statement, vinden wij hier de dichstbijzijnde deur
-                //en gebruiken die
-                closestDoor = null;
-                foreach (door door in FindObjectsOfType<door>())
+                if ((Input.GetAxisRaw("Horizontal")) == 1f || (Input.GetAxisRaw("Horizontal") == -1f))
                 {
-                    //zorgt dat de closestdoor niet null is en er geen exceptions worden gegooid.
-                    if (closestDoor == null)
+                    if (!Physics2D.OverlapCircle(walkDirection.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.05f, blockDetector))
                     {
-                        closestDoor = door;
+                        walkDirection.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                     }
 
-                    //als de door dichterbij is dan de tot nu closest door, verplaats de closest door dan
-                    else if (getDistance(door.gameObject) < getDistance(closestDoor.gameObject))
-                    {
-                        closestDoor = door;
-                    }
                 }
-                if (closestDoor.isOpen())
-                {
-                    walkDirection.position = closestDoor.otherSide.position;
-                    transform.position = closestDoor.otherSide.position;
-                }
-            }
 
-            //checkfortrainers
-            if (Physics2D.OverlapCircle(transform.position, 0.05f, trainers))
-            {
-                foreach (trainerInMap trainer in FindObjectsOfType<trainerInMap>())
+                else if ((Input.GetAxisRaw("Vertical")) == 1f || (Input.GetAxisRaw("Vertical") == -1f))
                 {
-                    //zorgt dat de closest trainer niet null is en er geen exceptions worden gegooid.
-                    if (closestTrainer == null)
+                    if (!Physics2D.OverlapCircle(walkDirection.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.05f, blockDetector))
                     {
-                        closestTrainer = trainer;
+                        walkDirection.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                     }
+                }
 
-                    else if (getDistance(trainer.gameObject) < getDistance(closestTrainer.gameObject))
+                //check For Doors
+                if (Physics2D.OverlapCircle(transform.position, 0.05f, SceneShifter))
+                {
+                    //omdat wij geen gameobject van de deur kunnen krijgen uit deze if statement, vinden wij hier de dichstbijzijnde deur
+                    //en gebruiken die
+                    closestDoor = null;
+                    foreach (door door in FindObjectsOfType<door>())
                     {
-                        closestTrainer = trainer;
+                        //zorgt dat de closestdoor niet null is en er geen exceptions worden gegooid.
+                        if (closestDoor == null)
+                        {
+                            closestDoor = door;
+                        }
+
+                        //als de door dichterbij is dan de tot nu closest door, verplaats de closest door dan
+                        else if (getDistance(door.gameObject) < getDistance(closestDoor.gameObject))
+                        {
+                            closestDoor = door;
+                        }
+                    }
+                    if (closestDoor.isOpen())
+                    {
+                        walkDirection.position = closestDoor.otherSide.position;
+                        transform.position = closestDoor.otherSide.position;
                     }
                 }
-                if (closestTrainer.hasBeenBeaten == false)
+
+                //checkfortrainers
+                if (Physics2D.OverlapCircle(transform.position, 0.05f, trainers))
                 {
-                    closestTrainer.battle();
-                }
-                else
-                {
-                    Debug.Log("blocking...");
-                    //makes sure that the door behaves as a blocker when closed
-                    walkDirection.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    foreach (trainerInMap trainer in FindObjectsOfType<trainerInMap>())
+                    {
+                        //zorgt dat de closest trainer niet null is en er geen exceptions worden gegooid.
+                        if (closestTrainer == null)
+                        {
+                            closestTrainer = trainer;
+                        }
+
+                        else if (getDistance(trainer.gameObject) < getDistance(closestTrainer.gameObject))
+                        {
+                            closestTrainer = trainer;
+                        }
+                    }
+                    if (closestTrainer.hasBeenBeaten == false)
+                    {
+                        closestTrainer.battle();
+                    }
+                    else
+                    {
+                        Debug.Log("blocking...");
+                        //makes sure that the door behaves as a blocker when closed
+                        walkDirection.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    }
                 }
             }
         }
-    }
-    float getDistance(GameObject target)
-    {
-        // ((x1-x2)^2+(y1-y2)^2)^0.5 <-- pythagoras formula for the distance 
-        return Mathf.Pow(Mathf.Pow(gameObject.transform.position.x - target.transform.position.x, 2) + Mathf.Pow(gameObject.transform.position.y - target.transform.position.y, 2), 0.5f);
+        float getDistance(GameObject target)
+        {
+            // ((x1-x2)^2+(y1-y2)^2)^0.5 <-- pythagoras formula for the distance 
+            return Mathf.Pow(Mathf.Pow(gameObject.transform.position.x - target.transform.position.x, 2) + Mathf.Pow(gameObject.transform.position.y - target.transform.position.y, 2), 0.5f);
+        }
     }
 }
 
---
+++ New Unity Project (2)/Assets/Go.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour
+    
+{
+    bool checker = false;
+    float speed = 6f;
+    bool isnottriggered = true;
+    public LayerMask Challanger;
+    public Transform stand;
+    private Vector2 position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        position = gameObject.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        stand.position = new Vector3(23.85f, 71f, 0f);
+
+        if (Physics2D.OverlapCircle(new Vector3(24f, 70f, 0f), 0.8f, Challanger) && isnottriggered == true)
+        {
+            checker = true;
+        }
+        if(checker == true)
+        { 
+            Debug.Log("Hij heeft hem hoor");
+            isnottriggered = false;
+            transform.position = Vector3.MoveTowards(transform.position, stand.position, speed * Time.deltaTime);
+            if(transform.position != stand.position) 
+            {
+                GameObject player = GameObject.Find("player");
+                Bewegingsemulgator bewegingsemulgator = player.GetComponent<Bewegingsemulgator>();
+                bewegingsemulgator.freeze = true;
+            }
+            else if(transform.position == stand.position)
+            {
+                GameObject player = GameObject.Find("player");
+                Bewegingsemulgator bewegingsemulgator = player.GetComponent<Bewegingsemulgator>();
+                bewegingsemulgator.freeze = false;
+            }
+        }
+    }
+}
